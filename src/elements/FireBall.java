95e35061ade49c701afbb41073621d2e88d2e560
unknown <tongxiqing@ZTE9-user32.zte9.local>
2015-02-27 17:46:55 +0800

    fire ball

--- src/elements/FireBall.java
+++ src/elements/FireBall.java
@@ -9,20 +9,37 @@ import javax.microedition.lcdui.Graphics;
  */
 public class FireBall implements Effect{
 
-    private Motion fire_ball_motion;
+    private Motion[] fire_ball_motion;
 
     public FireBall() {
-        fire_ball_motion = new Motion("/effect2/fireball/fireball.anu",50,50);
-        fire_ball_motion.keepId(0);
+
+        fire_ball_motion = new Motion[4];
+
+        for(int i=0; i<fire_ball_motion.length; i++){
+            fire_ball_motion[i] = new Motion("/effect2/fireball/fireball.anu",50,50);
+            fire_ball_motion[i].keepId(0);
+        }
     }
 
     public void update(int falling_fire_y) {
-        fire_ball_motion.keepId(0);
-        fire_ball_motion.update(falling_fire_y,200);
+        fire_ball_motion[0].keepId(0);
+        fire_ball_motion[0].update(falling_fire_y, 100);
+
+        fire_ball_motion[1].keepId(0);
+        fire_ball_motion[1].update(falling_fire_y, 300);
+
+        fire_ball_motion[2].keepId(0);
+        fire_ball_motion[2].update(falling_fire_y+100, 100);
+
+        fire_ball_motion[3].keepId(0);
+        fire_ball_motion[3].update(falling_fire_y+100, 300);
     }
 
     public void draw(Graphics g) {
-        fire_ball_motion.draw(g);
+        fire_ball_motion[0].draw(g);
+        fire_ball_motion[1].draw(g);
+        fire_ball_motion[2].draw(g);
+        fire_ball_motion[3].draw(g);
     }
 
 }
