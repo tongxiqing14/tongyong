117f5e0d9d16b76bf8bd800c5bd1d2b95ecdb1de
TongXiqing <418525836@qq.com>
2015-04-08 17:42:51 +0800

    PointCoco
    PointCoco

--- src/elements/PointCoco.java
+++ src/elements/PointCoco.java
@@ -11,6 +11,7 @@ import javax.microedition.lcdui.Graphics;
 public class PointCoco implements Effect{
 
     private Motion[] point_fire_motion;
+    private int frameCount = 0;
     private int falling_fire_x;
     private int[] falling_fire_x_array = {350,430,430,510,510};
     private int[] falling_fire_y_array = {250,200,300,180,320};
@@ -34,17 +35,25 @@ public class PointCoco implements Effect{
 
     public void update(int falling_fire_y) {
 
-        for (int i=0; i<point_fire_motion.length; i++){
-            if(i<WorriesFightScreen.pos_y_array.length)
-                WorriesFightScreen.pos_y_array[i] = point_fire_motion[i].getCollied(0)[1]+point_fire_motion[i].getSpriteDrawY();
-            point_fire_motion[i].keepId(0);
-            point_fire_motion[i].update(falling_fire_x_array[i], falling_fire_y_array[i]);
+        if(frameCount < 2*point_fire_motion[0].getCountFrame()){
+            for (int i=0; i<point_fire_motion.length; i++){
+                if(i<WorriesFightScreen.pos_y_array.length)
+                    WorriesFightScreen.pos_y_array[i] = point_fire_motion[i].getCollied(0)[1]+point_fire_motion[i].getSpriteDrawY();
+                point_fire_motion[i].keepId(0);
+                point_fire_motion[i].update(falling_fire_x_array[i], falling_fire_y_array[i]);
+            }
+            frameCount ++;
         }
+
     }
 
     public void draw(Graphics g) {
-        for (int i=0; i<point_fire_motion.length; i++){
-            point_fire_motion[i].draw(g);
+
+        if(frameCount < 2*point_fire_motion[0].getCountFrame()){
+            for (int i=0; i<point_fire_motion.length; i++){
+                point_fire_motion[i].draw(g);
+            }
         }
+
     }
 }
